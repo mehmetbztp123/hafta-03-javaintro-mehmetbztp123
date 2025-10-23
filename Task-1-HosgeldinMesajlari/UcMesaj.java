ace069d044d55a7f27b4a818f30db54ef73de83f
mehmetbztp123 <240541084@firat.edu.tr>
2025-10-23 20:36:12 +0300

    UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,7 @@
+public class UcMesaj {
+    public static void main(String[] args) {
+        System.out.println("Javaya Hos Geldiniz");
+        System.out.println("Bilgisayar Bilimine Hos Geldiniz");
+        System.out.println("Programlama Eglencelidir");
+    }
+}
