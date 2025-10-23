792f533c3ac99504a030a917fd6b20d25d0991c6
mehmetbztp123 <240541084@firat.edu.tr>
2025-10-23 21:34:51 +0300

    AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,64 @@
+public class AsciiArtIsim {
+    public static void main(String[] args) {
+        System.out.println("--- ASCII ART: MEHMET ---");
+        System.out.println(); // Bos satir
+
+        // Harf M
+        System.out.println("M   M EEE H   H M   M EEE TTTTT ");
+        System.out.println("MM MM E   H   H MM MM E     T   ");
+        System.out.println("M M M EEE HHHHH M M M EEE   T   ");
+        System.out.println("M   M E   H   H M   M E     T   ");
+        System.out.println("M   M EEE H   H M   M EEE   T   ");
+        System.out.println(); // Bos satir
+        
+        /*
+        // Alternatif olarak, her harfi ayri ayri satirlar halinde de yapabiliriz.
+        // Ornegin sadece 'M' harfi icin:
+        System.out.println("M   M");
+        System.out.println("MM MM");
+        System.out.println("M M M");
+        System.out.println("M   M");
+        System.out.println("M   M");
+        System.out.println(); // Bos satir
+
+        // Harf E
+        System.out.println("EEEEE");
+        System.out.println("E    ");
+        System.out.println("EEEEE");
+        System.out.println("E    ");
+        System.out.println("EEEEE");
+        System.out.println(); // Bos satir
+        
+        // Harf H
+        System.out.println("H   H");
+        System.out.println("H   H");
+        System.out.println("HHHHH");
+        System.out.println("H   H");
+        System.out.println("H   H");
+        System.out.println(); // Bos satir
+
+        // Tekrar M
+        System.out.println("M   M");
+        System.out.println("MM MM");
+        System.out.println("M M M");
+        System.out.println("M   M");
+        System.out.println("M   M");
+        System.out.println(); // Bos satir
+
+        // Tekrar E
+        System.out.println("EEEEE");
+        System.out.println("E    ");
+        System.out.println("EEEEE");
+        System.out.println("E    ");
+        System.out.println("EEEEE");
+        System.out.println(); // Bos satir
+        
+        // Harf T
+        System.out.println("TTTTT");
+        System.out.println("  T  ");
+        System.out.println("  T  ");
+        System.out.println("  T  ");
+        System.out.println("  T  ");
+        */
+    }
+}
