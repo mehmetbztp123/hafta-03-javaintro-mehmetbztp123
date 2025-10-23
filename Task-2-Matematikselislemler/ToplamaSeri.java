f9f5f6ff8a90ba9d086e632d15b4aa2b94d3514c
mehmetbztp123 <240541084@firat.edu.tr>
2025-10-23 20:45:15 +0300

    ToplamaSeri.java

---
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -0,0 +1,12 @@
+public class ToplamaSeri {
+    public static void main(String[] args) {
+        
+        int toplam = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9;
+        
+        
+        
+        System.out.println("1'den 9'a kadar olan sayilarin toplami: " + toplam);
+        
+       
+    }
+}
