fa7d28f2b1d1e9d5f0fb82abe79f3889a327332b
mehmetbztp123 <240541084@firat.edu.tr>
2025-10-23 21:09:01 +0300

    AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,10 @@
+public class AsciiArtHarf {
+    public static void main(String[] args) {
+        System.out.println("--- ASCII ART: 'A' HARFI ---");
+        System.out.println("      /\\     ");
+        System.out.println("     /  \\    ");
+        System.out.println("    /____\\   ");
+        System.out.println("   /      \\  ");
+        System.out.println("  /        \\ ");
+    }
+}
